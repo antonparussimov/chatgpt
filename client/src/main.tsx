b032c51f40fd1409a2e2eb1fc897b1567e19a8a8
bluedream7004 <kazimierz9974@gmail.com>
2023-06-11 16:25:57 +0900

    add rtk

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,13 @@ import ReactDOM from 'react-dom/client'
 import {
   RouterProvider,
 } from "react-router-dom";
+import { Provider } from 'react-redux';
+import { store } from './store'
 import App from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <RouterProvider router={App} />
+  <Provider store={store}>
+    <RouterProvider router={App} />
+  </Provider>
 )
